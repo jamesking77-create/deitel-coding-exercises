7bbe552d8d40c9fd85b09b615c027eabf53bd9fa
james king <jamesasuelimen77@gmail.com>
2023-04-06 20:14:08 +0100

    initial commit

--- src/RandomPractice/PizzaHut.java
+++ src/RandomPractice/PizzaHut.java
@@ -1,15 +1,11 @@
 package RandomPractice;
    
-public class PizzaHot {
+public class PizzaHut {
     public static void main(String[] args) {
-        pizzaCheck(14, 6, 4);
+        pizzaCheck(11, 6, 4);
     }
 
     private static void pizzaCheck(int numberOfPeople, int maxSatifaction, int minSatisfaction) {
-        minSatisfaction = 2 * minSatisfaction;
-        maxSatifaction = 3 * maxSatifaction;
-        int amountOfPizza = numberOfPeople + (maxSatifaction  + (minSatisfaction / 2) + (minSatisfaction));
-        System.out.println(amountOfPizza);
 
     }
 }
